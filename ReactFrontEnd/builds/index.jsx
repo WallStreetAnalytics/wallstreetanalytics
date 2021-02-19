e221c5545d6f1b8d9af2e6b25076db4d2114727b
Joe Ryan <joe.ryan@creativeapestudios.com>
2021-02-18 23:32:58 -0600

    saw that you are talking to investors and i am no longer interested. do not use this code.

--- ReactFrontEnd/builds/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import IndexPage from '../src/build_entries/index.jsx'
-
-ReactDOM.render( <IndexPage />, document.getElementById('index-entry-mount'))
